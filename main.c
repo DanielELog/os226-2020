d08993517fae055945a4ea9051fab1cfd6060963
DanielELog <43185213+DanielELog@users.noreply.github.com>
2020-09-18 01:31:11 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int lastReturn = 0;
+int LINE_LIMIT = 256;
+int ARG_LIMIT = 256;
+
+char *CMD_DELIMETERS = ";\n";
+char *ARG_DELIMETERS = " ";
+
+char *RETCODE_NAME = "retcode";
+char *ECHO_NAME = "echo";
+
 int echo(int argc, char *argv[]) {
 	for (int i = 1; i < argc; ++i) {
 		printf("%s%c", argv[i], i == argc - 1 ? '\n' : ' ');
@@ -12,8 +22,56 @@ int echo(int argc, char *argv[]) {
 }
 
 int retcode(int argc, char *argv[]) {
+	if (argc > 1) {
+		printf("No arguments expected for retcode to get\n");
+		return 1;
+	}
+	printf("%d\n", lastReturn);
+	return 0;
 }
 
 int main(int argc, char *argv[]) {
+	char line[LINE_LIMIT];
+	
+	char *command;
+	char *cmdArgv[ARG_LIMIT];
+	int cmdArgc = 0;
+	
+	char *argument;
+	
+	char *lineTokenPtr = NULL;
+	char *commandTokenPtr = NULL;
+	
+	int (*cmdToCall)(int, char **);
+	
+	while (fgets(line, LINE_LIMIT, stdin)) {
+		command = strtok_r(line, CMD_DELIMETERS, &lineTokenPtr);
+		while (command != NULL) {
+			cmdArgc = 0;
+			
+			argument = strtok_r(command, ARG_DELIMETERS, &commandTokenPtr);
+			while (argument != NULL && cmdArgc < 256) {
+				cmdArgv[cmdArgc] = argument;
+				cmdArgc++;
+				argument = strtok_r(command, ARG_DELIMETERS, &commandTokenPtr);
+			}
+			
+			command = strtok_r(line, CMD_DELIMETERS, &lineTokenPtr);
+			
+			// we got an empty command
+			if (cmdArgc == 0)
+				continue;
+			
+			if (strcmp(cmdArgv[0], ECHO_NAME))
+				cmdToCall = &echo;
+			else if (strcmp(cmdArgv[0], RETCODE_NAME))
+				cmdToCall = &retcode;
+			else
+				printf("Unknown command: %s\n", cmdArgv[0]);
+			
+			lastReturn = cmdToCall(cmdArgc, cmdArgv);
+		}
+	}
+	
 	return 0;
 }
